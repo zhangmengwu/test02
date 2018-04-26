5ae55bcbd7a4fbc5c32745738c857801e282f661
zhangmeng <1246184492@qq.com>
2018-04-26 12:40:05 +0800

    提交footer

---
+++ src/components/footer/my-footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+      我是头部
+      我是第二次修改
+
+      头部内容添加
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "my-footer"
+    }
+</script>
+
+<style scoped>
+  .header{
+    text-align: center;
+    color:red;
+  }
+</style>
